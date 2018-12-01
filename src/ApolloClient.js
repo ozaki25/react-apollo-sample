1e9d93dc5802d62e02ca1dc421f9ffab2a43d43b
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-02 01:06:23 +0900

    apollo hello

---
+++ src/ApolloClient.js
@@ -0,0 +1,38 @@
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
+
+async function run() {
+  const result = await client.query({ query });
+  console.log(result);
+}
+
+export default { run };
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from './ApolloClient';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <button onClick={ApolloClient.run}>ボタン</button>
         </header>
       </div>
     );
